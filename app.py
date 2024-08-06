d89c0ff05d5a74fde622b75d74de9dd1bbf706fa
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2024-08-06 14:32:58 +0000

    salvando as rotas

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,4 +6,35 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route ('/conheca')
+def conheca():
+    return render_template('conheca.html')
 
+@app.route ('/nossos_trabalhos')
+def nossos_trabalhos():
+    return render_template('nossos_trabalhos.html')
+
+@app.route('/lovelace')
+def lovelace():
+    return render_template('lovelace.html')
+
+@app.route('/dados')
+def dados():
+    return render_template('dados.html')
+
+@app.route('/login', methods=['post', "get"])
+def login():
+    return render_template('login.html')
+
+@app.route('/rede_de_apoio', methods=['post'])
+def rede_de_apoio():
+    usuario = request.form["usuario"]
+    senha = request.form["senha"]
+    if usuario== "alba.lopes" and senha=="12345":
+        return render_template('rede_de_apoio.html', usuario=usuario)
+    else:
+        return "Usuário ou senha incorretos"
+    
+@app.route('/cadastro')
+def cadastro():
+    return render_template("cadastro.html")
