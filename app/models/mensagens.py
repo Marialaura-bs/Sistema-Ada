82b0ebf90aa5d0d725f7bbc128dcbb9b0f752a80
Marialaura-bs <133356931+Marialaura-bs@users.noreply.github.com>
2025-02-20 23:25:39 +0000

    salvando

--- app/models/mensagens.py
+++ app/models/mensagens.py
@@ -1,19 +1,16 @@
 from config import db
 from flask_login import UserMixin
-from app.models.usuario import User
-   
+
 class Mensagens(db.Model, UserMixin):
-    __tablename__ = "mensagens"  # nome da tabela no banco de dados
+    __tablename__ = "mensagens"
     id = db.Column(db.Integer, primary_key=True, nullable=False, unique=True)
     mensagem = db.Column(db.String(500), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Relacionamento com User
-    user = db.relationship('User', backref=db.backref('mensagens', lazy=True))  # Definindo o relacionamento com a tabela User
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id', name='fk_mensagens_user_id'), nullable=True)  # Definir um nome para a restrição de chave estrangeira
+    user = db.relationship('User', backref=db.backref('mensagens', lazy=True))
 
-    # Construtor para inicializar a mensagem
     def __init__(self, mensagem=None, user_id=None):
         self.mensagem = mensagem
         self.user_id = user_id
     
-    # Representação da classe
     def __repr__(self):
         return f'<Mensagens {self.mensagem}>'
